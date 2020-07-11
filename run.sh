7b630fe5be1aa4cb73929bdb3254667259df3798
fChristenson <fredrik.christenson@gmail.com>
2020-07-11 17:47:17 +0200

    wp

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo PORT=80 /home/ubuntu/.nvm/versions/node/v14.5.0/bin/node server.js
+PORT=80 /home/ubuntu/.nvm/versions/node/v14.5.0/bin/node server.js
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
