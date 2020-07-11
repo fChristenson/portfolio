51f759aea79d604442bc7237ec6ef275692708da
fChristenson <fredrik.christenson@gmail.com>
2020-07-11 16:48:34 +0200

    wp

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash) && nvm install node
+(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash) && bash nvm install node
