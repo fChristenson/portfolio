4c0d3207de7a3b114c2b525255e75eee0c36f828
fChristenson <fredrik.christenson@gmail.com>
2020-07-12 10:57:09 +0200

    wp

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo mv ./portfolio.service /etc/systemd/system/
+sudo cp ./portfolio.service /etc/systemd/system/
 sudo systemctl enable portfolio.service
 sudo systemctl start portfolio.service
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
